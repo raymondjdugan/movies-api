70cd7f8366e2231eedff8aca71762e0794586c30
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-04-04 15:24:54 -0500

    Completed and tested doPost

---
+++ src/main/java/data/Movie.java
@@ -0,0 +1,131 @@
+package data;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String director;
+    private String poster;
+    private String dateReleased;
+    private int yearMade;
+    private String genre;
+    private String plot;
+    private String rating;
+    private double imdb;
+    private int runtime;
+    private String actors;
+
+    public Movie() {
+
+    }
+
+    public Movie(int id, String title, String director, String poster, String dateReleased, int yearMade, String genre, String plot, String rating, double imdb, int runtime, String actors) {
+        this.id = id;
+        this.title = title;
+        this.director = director;
+        this.poster = poster;
+        this.dateReleased = dateReleased;
+        this.yearMade = yearMade;
+        this.genre = genre;
+        this.plot = plot;
+        this.rating = rating;
+        this.imdb = imdb;
+        this.runtime = runtime;
+        this.actors = actors;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getDateReleased() {
+        return dateReleased;
+    }
+
+    public void setDateReleased(String dateReleased) {
+        this.dateReleased = dateReleased;
+    }
+
+    public int getYearMade() {
+        return yearMade;
+    }
+
+    public void setYearMade(int yearMade) {
+        this.yearMade = yearMade;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public double getImdb() {
+        return imdb;
+    }
+
+    public void setImdb(double imdb) {
+        this.imdb = imdb;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+}
---
+++ src/main/java/main/MovieServlet.java
@@ -0,0 +1,68 @@
+package main;
+
+import com.google.gson.Gson;
+import data.Movie;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+@WebServlet(name = "MovieServlet", urlPatterns = "/movies/*")
+public class MovieServlet extends HttpServlet {
+    Movie movie = new Movie(4,
+            "Gangs of New York",
+            "Martin Scorsese",
+            "https://m.media-amazon.com/images/M/MV5BNDg3MmI1ZDYtMDZjYi00ZWRlLTk4NzEtZjY4Y2U0NjE5YmRiXkEyXkFqcGdeQXVyNzAxMjE1NDg@._V1_SX300.jpg",
+            "20 Dec 2002",
+            2001,
+            "Crime, Drama",
+            "In 1862, Amsterdam Vallon returns to the Five Points area of New York City seeking revenge against Bill the Butcher, his " +
+                    "father's killer.",
+            "R",
+            7.5,
+            167,
+            "Leonardo DiCaprio, Cameron Diaz, Daniel Day-Lewis");
+
+    ArrayList<Movie> movies = new ArrayList<>();
+    int movieId = 1;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+
+        try {
+            PrintWriter out = response.getWriter();
+            String movieString = new Gson().toJson(movies.toArray());
+            out.println(movieString);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+
+        Movie[] newMovies = new Gson().fromJson(request.getReader(), Movie[].class);
+        for (Movie movie: newMovies){
+            movie.setId(movieId++);
+            movies.add(movie);
+        }
+
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("Added Movies");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
